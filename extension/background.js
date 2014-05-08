2a3f7d256e70cffc6f9eb8957670b7af6f6e4e22
Mark Williams <m.williams@contentbloom.com>
2014-05-08 15:14:07 +0100

    Added loading and loaded dialog boxes

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,30 @@
-var articleContent = "";
-jQuery(document).ready(function(){
-    if($('.page-nav-post a').length > 0) {
+var loading_message = "\
+<p>This article has multiple pages.</p>\
+<img src=\"http://mtrl.co.uk/img/loading.gif\" class=\"ofg-loading\">\
+<p>Currently loading all article content onto this page...</p>\
+";
+
+var ofg_css = "\
+.ofg-one-page-loading {\
+    width: 400px;\
+    border-radius: 10px;\
+    display: block;\
+    margin: 20px auto;\
+    background: #efefef;\
+    padding: 10px;\
+    text-align: center;\
+}\
+\
+img.ofg-loading-img {\
+    width: 30px;\
+    height: auto;\
+}\
+";
+
+jQuery.fn.make_one_page = function() {
+    var articleContent = "";
+    if(jQuery('.page-nav-post a').length > 0) {
+        jQuery.fn.loading(true);
         var links = jQuery('.page-nav-post a');
         links.each(function() {
             if(jQuery(this).text().trim() != 'Next') {
@@ -11,8 +35,26 @@ jQuery(document).ready(function(){
                     jQuery('article footer, .td-more-articles-box, article header').hide();
                     jQuery('article header:first-child, article footer:last-child').show();
                     jQuery('.page-nav-post').hide();
+                    // Show loaded
+                    jQuery.fn.loading(false);
                 });
             }
         });
     }
+    return this;
+}
+
+jQuery.fn.loading = function(do_show) {
+    // Add css
+    if(do_show){
+        $('head').append('<style>' + ofg_css + '</style>');
+        $('<div class="ofg-one-page-loading">' + loading_message + '</div>').insertAfter('article header:first-child');
+    } else {
+        $('.ofg-one-page-loading').html('Loaded content');
+        $('.ofg-one-page-loading').fadeOut(1000);
+    }
+}
+
+jQuery(document).ready(function(){
+    jQuery.fn.make_one_page();
 });
